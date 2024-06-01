358a95054d4b57ad9a941e1065a70d5877ebd6ba
Benno <81779508+Westpol@users.noreply.github.com>
2024-06-01 19:12:56 +0200

    Added all needed Libraries
    Servo: added motor set to 0

--- fc_chip_firmware/fc_chip_firmware.ino
+++ fc_chip_firmware/fc_chip_firmware.ino
@@ -1,41 +1,48 @@
+#include <SPI.h>
 #include "Wire.h"
 #include "SoftwareSerial.h"
 
 #include <MPU6050_light.h>
 #include <Adafruit_BMP280.h>
+#include <SD.h>
+#include "sbus.h"
 
 MPU6050 mpu(Wire);
+
 SoftwareSerial dataBus(10, 9);
 
-float setpoint = 0;
-float middles[6] = {0, 0, 1500, 1500, 1500, 1500};
+bfs::SbusRx sbus_rx(&Serial);
+bfs::SbusData data;
+
+float offsets[6] = {0, 0, 0, 0, 0, 0};
 float servoSetpoints[6] = {middles[0], middles[1], middles[2], middles[3], middles[4], middles[5]};
 float servoDirectionsYaw[6] = {0, 0, 1, 1, 1, 1};
-float gyroX;
+float gyroX, gyroY, gyroZ;
 
 
 void setup() {
+
+  sbus_rx.Begin();
+
   dataBus.begin(115200);
-  Serial.begin(115200);
   Wire.begin();
   
   byte status = mpu.begin();
-  Serial.print(F("MPU6050 status: "));
-  Serial.println(status);
   while(status!=0){ } // stop everything if unable to connect to MPU6050
-  
-  Serial.println(F("Calculating offsets, do not move MPU6050"));
   delay(1000);
   mpu.calcOffsets(true, true); // gyro and accelero
-  Serial.println("Done!\n");
 }
 
 void loop() {
+  if (sbus_rx.Read()) {
+    data = sbus_rx.data();
+  }
+
   mpu.update();
   gyroX = mpu.getGyroX();
   gyroY = mpu.getGyroY();
   gyroZ = mpu.getGyroZ();
-  float p_value = setpoint - (gyroX * 2); 
+  float p_value = -(gyroX * 2); 
   for(int i = 0; i <= 3; i++){
     servoSetpoints[i] = minmax(middles[i] + (p_value * servoDirectionsYaw[i]), 1100, 1900);
   }
