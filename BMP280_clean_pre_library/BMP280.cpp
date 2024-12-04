afa7b2c86b4a70a3a41f5bdde3b1973c3c28b3e6
Benno <81779508+Westpol@users.noreply.github.com>
2024-12-04 11:23:23 +0100

    Added basic library functions

---
+++ BMP280_clean_pre_library/BMP280.cpp
@@ -0,0 +1,137 @@
+#include "BMP280.h"
+#include "Arduino.h"
+#include "SPI.h"
+
+
+BMP280::BMP280(unsigned long spi_speed, int cs){
+  _spi_speed = spi_speed;
+  _cs = cs;
+}
+
+
+int BMP280::init(){
+  pinMode(_cs, OUTPUT);
+  digitalWrite(_cs, HIGH);
+  SPI.begin();
+  SPI.beginTransaction(SPISettings(_spi_speed, MSBFIRST, SPI_MODE0));
+
+  digitalWrite(_cs, LOW);   // check if chip has the correct ID
+  SPI.transfer(0xD0 | _readValue);
+  unsigned char id = SPI.transfer(0);
+  digitalWrite(_cs, HIGH);
+  if(id != 0x58){
+    return 0;
+  }
+
+  getConstants();
+
+  digitalWrite(_cs, LOW);   // write mode and settings to chip
+  SPI.transfer(0xF4 & _writeValue);
+  SPI.transfer(_mode | _osrs_p | _osrs_t);
+  digitalWrite(_cs, HIGH);
+  return 1;
+}
+
+
+void BMP280::update(){
+  unsigned char* bytes = (unsigned char*)malloc(sizeof(unsigned char) * 6);
+  SPI.transfer(0xF7);
+  read_continuous(bytes, 6);
+  _press = ((uint32_t)*(bytes) << 12) + ((uint32_t)*(bytes + 1) << 4) + ((uint32_t)*(bytes + 2) >> 4);
+  _temp = ((uint32_t)*(bytes + 3) << 12) + ((uint32_t)*(bytes + 4) << 4) + ((uint32_t)*(bytes + 5) >> 4);
+  free(bytes);
+}
+
+
+void BMP280::updateTemp(){
+  // get raw temperature values
+  unsigned char* bytes = (unsigned char*)malloc(sizeof(unsigned char) * 3);
+  SPI.transfer(0xFA);
+  read_continuous(bytes, 3);
+  _temp = ((uint32_t)*(bytes) << 12) + ((uint32_t)*(bytes + 1) << 4) + ((uint32_t)*(bytes + 2) >> 4);
+  free(bytes);
+}
+
+
+void BMP280::updatePress(){
+  // get raw pressure values
+  unsigned char* bytes = (unsigned char*)malloc(sizeof(unsigned char) * 3);
+  SPI.transfer(0xF7);
+  read_continuous(bytes, 3);
+  _press = ((uint32_t)*(bytes) << 12) + ((uint32_t)*(bytes + 1) << 4) + ((uint32_t)*(bytes + 2) >> 4);
+  free(bytes);
+}
+
+
+double BMP280::getTemp(){
+  return _temp;
+}
+
+
+double BMP280::getPress(){
+  return _press;
+}
+
+
+void BMP280::read_continuous(unsigned char* bytes, int len){
+  for(int i = 0; i < len; i++){
+    *(bytes + i) = SPI.transfer(0);
+  }
+}
+
+void BMP280::getConstants(){
+
+  // get temperature constants
+  unsigned char val1, val2;
+  SPI.transfer(0x88 | _readValue);
+  val1 = SPI.transfer(0);
+  val2 = SPI.transfer(0);
+  _dig_T1 = ((uint16_t)val1 << 8) + val2;
+  SPI.transfer(0x8A | _readValue);
+  val1 = SPI.transfer(0);
+  val2 = SPI.transfer(0);
+  _dig_T2 = ((uint16_t)val1 << 8) + val2;
+  SPI.transfer(0x8C | _readValue);
+  val1 = SPI.transfer(0);
+  val2 = SPI.transfer(0);
+  _dig_T3 = ((uint16_t)val1 << 8) + val2;
+
+
+  // get pressure constants
+  SPI.transfer(0x8E | _readValue);
+  val1 = SPI.transfer(0);
+  val2 = SPI.transfer(0);
+  _dig_P1 = ((uint16_t)val1 << 8) + val2;
+  SPI.transfer(0x90 | _readValue);
+  val1 = SPI.transfer(0);
+  val2 = SPI.transfer(0);
+  _dig_P2 = ((uint16_t)val1 << 8) + val2;
+  SPI.transfer(0x92 | _readValue);
+  val1 = SPI.transfer(0);
+  val2 = SPI.transfer(0);
+  _dig_P3 = ((uint16_t)val1 << 8) + val2;
+  SPI.transfer(0x94 | _readValue);
+  val1 = SPI.transfer(0);
+  val2 = SPI.transfer(0);
+  _dig_P4 = ((uint16_t)val1 << 8) + val2;
+  SPI.transfer(0x96 | _readValue);
+  val1 = SPI.transfer(0);
+  val2 = SPI.transfer(0);
+  _dig_P5 = ((uint16_t)val1 << 8) + val2;
+  SPI.transfer(0x98 | _readValue);
+  val1 = SPI.transfer(0);
+  val2 = SPI.transfer(0);
+  _dig_P6 = ((uint16_t)val1 << 8) + val2;
+  SPI.transfer(0x9A | _readValue);
+  val1 = SPI.transfer(0);
+  val2 = SPI.transfer(0);
+  _dig_P7 = ((uint16_t)val1 << 8) + val2;
+  SPI.transfer(0x9C | _readValue);
+  val1 = SPI.transfer(0);
+  val2 = SPI.transfer(0);
+  _dig_P8 = ((uint16_t)val1 << 8) + val2;
+  SPI.transfer(0x9E | _readValue);
+  val1 = SPI.transfer(0);
+  val2 = SPI.transfer(0);
+  _dig_P9 = ((uint16_t)val1 << 8) + val2;
+}
---
+++ BMP280_clean_pre_library/BMP280.h
@@ -0,0 +1,56 @@
+#include <stdint.h>
+#ifndef BMP280_h
+
+#define BMP280_h
+
+
+#include "Arduino.h"
+
+class BMP280{
+  public:
+  BMP280(unsigned long speed, int cs);
+  int init();
+  void update();
+  void updateTemp();
+  void updatePress();
+  double getTemp();
+  double getPress();
+
+  private:
+  unsigned long _spi_speed;
+  int _cs;
+
+  unsigned char _readValue = 0x80;
+  unsigned char _writeValue = 0x7F;
+
+  // Mode setup normal, 16x press, 16x temp
+  unsigned char _mode = B00000011;  // Normal Mode [1,1]
+  unsigned char _osrs_p = B00010100; // oversampling x16 [1, 0, 1]
+  unsigned char _osrs_t = B10100000; // oversampling x16 [1, 0, 1]
+
+  double _temp;
+  double _press;
+  uint32_t _rawTemp;
+  uint32_t _rawPress;
+
+  // constant calibration values temperature
+  uint16_t _dig_T1;
+  int16_t _dig_T2;
+  int16_t _dig_T3;
+
+  // constant calibration values pressure
+  uint16_t _dig_P1;
+  int16_t _dig_P2;
+  int16_t _dig_P3;
+  int16_t _dig_P4;
+  int16_t _dig_P5;
+  int16_t _dig_P6;
+  int16_t _dig_P7;
+  int16_t _dig_P8;
+  int16_t _dig_P9;
+
+  void read_continuous(unsigned char* bytes, int len);
+  void getConstants();
+};
+
+#endif
