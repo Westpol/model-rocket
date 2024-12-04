87e074186459b2737124a2c5d583a0efc08614d7
Benno <81779508+Westpol@users.noreply.github.com>
2024-12-04 11:55:00 +0100

    added pressure conversion to Pascal

--- BMP280_clean_pre_library/BMP280.cpp
+++ BMP280_clean_pre_library/BMP280.cpp
@@ -84,15 +84,36 @@ void BMP280::read_continuous(unsigned char* bytes, int len){
 
 void BMP280::convertData(){
   int32_t t_fine;
-  int32_t var1, var2, T;
+  int32_t tVar1, tVar2, T;
 
-  var1 = ((((_rawTemp >> 3) - ((int32_t)_dig_T1 << 1))) * ((int32_t)_dig_T2)) >> 11;
-  var2 = (((((_rawPress >> 4) - ((int32_t)_dig_T1)) * ((_rawPress >> 4) - ((int32_t)_dig_T1))) >> 12) * ((int32_t)_dig_T3)) >> 14;
+  tVar1 = ((((_rawTemp >> 3) - ((int32_t)_dig_T1 << 1))) * ((int32_t)_dig_T2)) >> 11;
+  tVar2 = (((((_rawPress >> 4) - ((int32_t)_dig_T1)) * ((_rawPress >> 4) - ((int32_t)_dig_T1))) >> 12) * ((int32_t)_dig_T3)) >> 14;
 
-  t_fine = var1 + var2;
+  t_fine = tVar1 + tVar2;
   T = (t_fine * 5 + 128) >> 8;
 
   _temp = T / 100.0;
+
+
+  int64_t var1, var2, p;
+  var1 = ((int64_t)t_fine) - 128000;
+  var2 = var1 * var1 * (int64_t)_dig_P6;
+  var2 = var2 + ((var1 * (int64_t)_dig_P5) << 17);
+  var2 = var2 + (((int64_t)_dig_P4) << 35);
+  var1 = ((var1 * var1 * (int64_t)_dig_P3) >> 8) + ((var1 * (int64_t)_dig_P2) << 12);
+  var1 = (((((int64_t)1) << 47) + var1)) * ((int64_t)_dig_P1) >> 33;
+  if (var1 == 0)
+  {
+  _press = 0; // avoid exception caused by division by zero
+  }
+  else{
+    p = 1048576 - _rawPress;
+    p = (((p << 31) - var2) * 3125) / var1;
+    var1 = (((int64_t)_dig_P9) * (p >> 13) * (p >> 13)) >> 25;
+    var2 = (((int64_t)_dig_P8) * p) >> 19;
+    p = ((p + var1 + var2) >> 8) + (((int64_t)_dig_P7) << 4);
+    _press = p / 256.0;
+  }
 }
 
 void BMP280::getConstants(){
